0261833f2d68b57ca9c1ef51f3a7d92e28867ae5
Eduardo Nogueira <eduardodudunog@gmail.com>
2022-09-16 15:27:51 -0300

    chore: add typescript

--- src/server.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const app = express()
-
-app.get('/ads', () => {
-  console.log('Acessou ads!')
-})
-
-app.listen(3333, () => console.log('Server running at http://localhost:5050'))
